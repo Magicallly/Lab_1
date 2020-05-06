0a5fbada384d7f8263b8ba88fc95a04691e99045
Magicallly <63975139+Magicallly@users.noreply.github.com>
2020-05-06 23:18:50 +0300

    Add files via upload

---
+++ dop1.dpr
@@ -0,0 +1,25 @@
+﻿program dop1;
+{$APPTYPE CONSOLE}
+{$R *.res}
+var
+a: string[255];
+b: string[255];
+var i,k,Length1, Length2: integer;
+n: boolean;
+begin
+n:=false;
+writeln('Введите числа А и В');
+readln(a);
+readln(b);
+Length1:= Length (a);
+Length2:= Length (b);
+for i := 1 to Length1 do  // loop A
+  for k := 1 to Length2 do  // loop B
+    begin
+    if (a[i]=b[k]) and  (a[i] <> '.') and (a[i] <> '-')
+    then n:=true;
+    end;
+  if n then write('Есть совпадающие цифры')
+  else write('Совпадающих цифр нет');
+readln;
+end.
